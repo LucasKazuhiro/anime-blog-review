8304df18b4f39690149c43064036becdc3f781d0
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-24 20:27:19 -0300

    feat(app): add user menu to app

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserMenuComponent } from './user-menu/user-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

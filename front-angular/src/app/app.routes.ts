2402a040e8989578914015d3d2625ef9bd219d14
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-25 20:28:09 -0300

    feat(routes): add new routes

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ReviewsComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];

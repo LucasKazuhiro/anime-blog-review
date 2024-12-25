20cb5e9c06231d0c5a41cac3a38c8d76b4ead865
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-25 01:15:08 -0300

    feat(app): add nav menu to the app component

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserMenuComponent } from './user-menu/user-menu.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserMenuComponent],
+  imports: [RouterOutlet, UserMenuComponent, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

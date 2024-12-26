786c0cef6c8677639cd5752d0e2b134bb81376ad
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-26 20:22:55 -0300

    feat(link-menu): create link menu compoment

---
+++ front-angular/src/app/link-menu/link-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'link-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './link-menu.component.html',
+  styleUrl: './link-menu.component.css'
+})
+export class LinkMenuComponent {
+
+}

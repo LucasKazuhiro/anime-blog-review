1a04482698819dabe56d7cb337d47530ea087a34
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-27 22:28:42 -0300

    fix(nav-menu): fix menu dot position

--- front-angular/src/app/nav-menu/nav-menu.component.ts
+++ front-angular/src/app/nav-menu/nav-menu.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './nav-menu.component.css'
 })
 export class NavMenuComponent {
+  calcCenterDot(btn_menu: EventTarget | null) {
+    // Check if EventTarget item is a HTMLElement
+    if (btn_menu instanceof HTMLElement) {
+      // Calculates the correct position of the dot
+      const btn_menu_width = btn_menu.offsetWidth;
+      const before_width = parseFloat(getComputedStyle(btn_menu, '::before').width);
+      const center_value = (btn_menu_width - before_width) / 2;
 
+      // Set the value in the translateX attribute
+      const btn_menu_id = document.getElementById(btn_menu.id)?.style;
+      if (btn_menu_id) {
+        btn_menu_id.setProperty('--var-move-dot', `translateX(${center_value}px)`)
+      }
+    }
+  }
 }

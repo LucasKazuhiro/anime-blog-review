af4699eb3de7d4652e92b7d598a13f27c49c9ad0
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-29 16:05:41 -0300

    feat(review-info): create review info component and display seached anime info

---
+++ front-angular/src/app/review-info/review-info.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimeService } from '../services/anime.service';
+import { Review } from '../models/review.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'review-info',
+  imports: [CommonModule],
+  templateUrl: './review-info.component.html',
+  styleUrl: './review-info.component.css'
+})
+export class ReviewInfoComponent implements OnInit {
+
+  public reviewInfo: Review = new Review;
+
+  constructor(private animeService:AnimeService){}
+
+  ngOnInit(){
+    // Get the saved review to display its data on the website
+    this.animeService.reviewSelected$.subscribe(reviewInfo => {
+      if (reviewInfo.id != '') {
+        this.reviewInfo = reviewInfo;
+      }
+      else {
+        this.reviewInfo = new Review;
+      }
+    })
+  }
+}

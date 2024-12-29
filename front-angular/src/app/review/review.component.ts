2e189258e0909fc52d6950699d060c545127c629
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-29 16:04:37 -0300

    feat(review): imports review info component

--- front-angular/src/app/review/review.component.ts
+++ front-angular/src/app/review/review.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { LinkMenuComponent } from '../link-menu/link-menu.component';
 import { MarkdownService } from 'ngx-markdown';
 import { ActivatedRoute } from '@angular/router';
 import { AnimeService } from '../services/anime.service';
-import { Review } from '../models/review.model';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { last, lastValueFrom } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
+import { ReviewInfoComponent } from '../review-info/review-info.component';
 
 @Component({
   selector: 'review',
-  imports: [LinkMenuComponent, CommonModule],
+  imports: [CommonModule, ReviewInfoComponent],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
 export class ReviewComponent implements OnInit {
 
   public reviewText: string | null = null;
-  public reviewInfo: Review | null = null;
 
   constructor(private route: ActivatedRoute, private http: HttpClient, private animeService: AnimeService, private mdService: MarkdownService) { }
 
@@ -32,21 +30,12 @@ export class ReviewComponent implements OnInit {
         this.reviewText = await this.mdService.parse(reviewText);
       }
       catch (error) {
+        this.reviewText = '';
         console.log("Error loading or parsing review text: ", error);
       }
 
       // Saves the searched review
       this.animeService.getReview(reviewId);
-
-      // Get the saved review to display its data on the website
-      this.animeService.reviewSelected$.subscribe(reviewInfo => {
-        if (reviewInfo.id != '') {
-          this.reviewInfo = reviewInfo;
-        }
-        else {
-          this.reviewInfo = null;
-        }
-      })
     }
 
   }

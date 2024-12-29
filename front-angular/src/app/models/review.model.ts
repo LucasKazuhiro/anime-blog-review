0626109add7bb323c2587307432aeab50a72d312
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-29 16:03:08 -0300

    fix(models): add attibutes

--- front-angular/src/app/models/review.model.ts
+++ front-angular/src/app/models/review.model.ts
@@ -1,16 +1,17 @@
 export class Review {
   public id: string = "";
-  public intro: string = "";
   public name: string = "";
+  public intro: string = "";
+  public rate: number = 0;
   public type: string = "";
   public episodes: number = 0;
   public studio: string = "";
   public source: string = "";
   public demographic: string = "";
+  public season: string = "";
   public startDate: string = "";
   public endDate: string = "";
   public genres: string[] = [];
-  public rate: number = 0;
   public reviewDate: string = "";
   public malLink: string = "";
   public anilistLink: string = "";

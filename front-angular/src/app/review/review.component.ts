da10da794560544b9fef92108480421ffc564347
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-29 13:57:36 -0300

    docs(review): add comments for readability

--- front-angular/src/app/review/review.component.ts
+++ front-angular/src/app/review/review.component.ts
@@ -6,7 +6,7 @@ import { AnimeService } from '../services/anime.service';
 import { Review } from '../models/review.model';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
+import { last, lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'review',
@@ -16,28 +16,29 @@ import { lastValueFrom } from 'rxjs';
 })
 export class ReviewComponent implements OnInit {
 
-  public reviewId: string | null = null;
   public reviewText: string | null = null;
   public reviewInfo: Review | null = null;
 
-  constructor(private route: ActivatedRoute, private animeService: AnimeService, private http: HttpClient, private mdService: MarkdownService) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private animeService: AnimeService, private mdService: MarkdownService) { }
 
   async ngOnInit() {
     const reviewId = this.route.snapshot.paramMap.get('id');
 
     if (reviewId) {
-      this.reviewId = reviewId;
-
       try {
+        // Get the text from the markdown file
         const reviewText = await lastValueFrom(this.http.get('/reviews_md/' + reviewId + '.md', { responseType: 'text' }));
+        // Parse the content into HTML (sanitizing if needed)
         this.reviewText = await this.mdService.parse(reviewText);
       }
       catch (error) {
         console.log("Error loading or parsing review text: ", error);
       }
 
-      this.animeService.getReview(this.reviewId);
+      // Saves the searched review
+      this.animeService.getReview(reviewId);
 
+      // Get the saved review to display its data on the website
       this.animeService.reviewSelected$.subscribe(reviewInfo => {
         if (reviewInfo.id != '') {
           this.reviewInfo = reviewInfo;

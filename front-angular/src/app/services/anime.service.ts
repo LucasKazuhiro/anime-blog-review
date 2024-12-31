f7f0c0d25f605ff6161763b1f3e5de19fd8c1a49
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-30 23:58:35 -0300

    feat(service): create function to get favorite by type

--- front-angular/src/app/services/anime.service.ts
+++ front-angular/src/app/services/anime.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, delay, map, Observable } from 'rxjs';
 import { Review } from '../models/review.model';
 import { HttpClient } from '@angular/common/http';
 import { Favorite } from '../models/favorite.model';
@@ -13,16 +13,40 @@ export class AnimeService {
 
   constructor(private http: HttpClient) { }
 
+  // Reviews
   // Create an Observable of type BehaviorSubject that will store the reviews banner array.
-  private reviewsBanner = new BehaviorSubject<Review[]>([]);
+  private reviewsBanner = new BehaviorSubject<Review[] | null>(null);
   // Create an Observable for reviewsBanner (read only)
   reviewsBanner$ = this.reviewsBanner.asObservable();
 
   private reviewSelected = new BehaviorSubject<Review>(new Review);
   reviewSelected$ = this.reviewSelected.asObservable();
 
+
+  // Favorites
   private favoriteTvs = new BehaviorSubject<Favorite[]>([]);
   favoriteTvs$ = this.favoriteTvs.asObservable();
+
+  private favoriteFilms = new BehaviorSubject<Favorite[]>([]);
+  favoriteFilms$ = this.favoriteFilms.asObservable();
+
+  private favoriteCharsMale = new BehaviorSubject<Favorite[]>([]);
+  favoriteCharsMale$ = this.favoriteCharsMale.asObservable();
+
+  private favoriteCharsFemale = new BehaviorSubject<Favorite[]>([]);
+  favoriteCharsFemale$ = this.favoriteCharsFemale.asObservable();
+
+  private favoriteCharsNoIdea = new BehaviorSubject<Favorite[]>([]);
+  favoriteCharsNoIdea$ = this.favoriteCharsNoIdea.asObservable();
+
+  private favoriteSeiyuusMale = new BehaviorSubject<Favorite[]>([]);
+  favoriteSeiyuusMale$ = this.favoriteSeiyuusMale.asObservable();
+
+  private favoriteSeiyuusFemale = new BehaviorSubject<Favorite[]>([]);
+  favoriteSeiyuusFemale$ = this.favoriteSeiyuusFemale.asObservable();
+
+  private favoriteStudios = new BehaviorSubject<Favorite[]>([]);
+  favoriteStudios$ = this.favoriteStudios.asObservable();
 
 
   getAllReviews() {
@@ -55,7 +79,7 @@ export class AnimeService {
     this.reviewsBanner.pipe(
       map(reviews =>
         // Searches for the review that matches the review_id
-        reviews.find((review) => review.id === reviewId) || new Review())  // Fin
+        reviews?.find((review) => review.id === reviewId) || new Review())
     ).subscribe(review => {
       // Saves the found review
       this.reviewSelected.next(review);
@@ -65,7 +89,43 @@ export class AnimeService {
   getFavoritesByType(type: string) {
     this.http.get<Favorite[]>(`https://${this.backend_domain}/favorites/${type}`).subscribe({
       next: (data) => {
-        this.favoriteTvs.next(data);
+        switch (type) {
+          case 'tvs':
+            this.favoriteTvs.next(data);
+            break;
+
+          case 'films':
+            this.favoriteFilms.next(data);
+            break;
+
+          case 'chars_male':
+            this.favoriteCharsMale.next(data);
+            break;
+
+          case 'chars_female':
+            this.favoriteCharsFemale.next(data);
+            break;
+
+          case 'chars_no_idea':
+            this.favoriteCharsNoIdea.next(data);
+            break;
+
+          case 'seiyuus_male':
+            this.favoriteSeiyuusMale.next(data);
+            break;
+
+          case 'seiyuus_female':
+            this.favoriteSeiyuusFemale.next(data);
+            break;
+
+          case 'studios':
+            this.favoriteStudios.next(data);
+            break;
+
+          default:
+            console.warn(`Unknown type: ${type}`);
+        }
+
       },
       error: (err) => {
         console.error(`Error on getFavoritesByType ${type}: `, err); // Error message

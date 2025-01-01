6ee607c5b27751794a3f0c1c32bdd95c4732c757
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-31 21:58:26 -0300

    feat(musics): add ending and ost, show more button

--- front-angular/src/app/musics/musics.component.ts
+++ front-angular/src/app/musics/musics.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { LinkMenuComponent } from '../link-menu/link-menu.component';
 import { Music } from '../models/music.model';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { AnimeService } from '../services/anime.service';
 import { CommonModule } from '@angular/common';
 
@@ -18,6 +18,14 @@ export class MusicsComponent {
 
   fav_types: string[] = ['op', 'ed', 'ost']
 
+  musicsOpsLoaded = 5;
+  musicsEdsLoaded = 5;
+  musicsOstsLoaded = 3;
+
+  musicsOpsTotal$: Observable<number>;
+  musicsEdsTotal$: Observable<number>;
+  musicsOstsTotal$: Observable<number>;
+
   constructor(private animeService: AnimeService) {
     this.fav_types.forEach(type => {
       animeService.getMusicsByType(type);
@@ -26,9 +34,29 @@ export class MusicsComponent {
     this.musicOps$ = this.animeService.musicOps$;
     this.musicEds$ = this.animeService.musicEds$;
     this.musicOsts$ = this.animeService.musicOsts$;
+
+    this.musicsOpsTotal$ = this.musicOps$.pipe(map(musics => musics?.length || 0));
+    this.musicsEdsTotal$ = this.musicEds$.pipe(map(musics => musics?.length || 0));
+    this.musicsOstsTotal$ = this.musicOsts$.pipe(map(musics => musics?.length || 0));
   }
 
-  navigateToMusicPage(musicName: string) {
-    window.open(`https://www.youtube.com/results?search_query=${musicName}`, '_blank');
+  navigateToMusicPage(musicName: string, musicAuthor: string) {
+    window.open(`https://www.youtube.com/results?search_query=${musicName}+by+${musicAuthor}`, '_blank');
+  }
+
+  showMore(type: string) {
+    switch (type) {
+      case "op":
+        this.musicsOpsLoaded += 5;
+        break;
+
+      case "ed":
+        this.musicsEdsLoaded += 5;
+        break;
+
+      case "ost":
+        this.musicsOstsLoaded += 5;
+        break;
+    }
   }
 }
